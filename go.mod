744255225ac540d1c3930df73a0aa933b67abec9
Thorsten Klöhn <thorstenkloehn@gmail.com>
2024-05-27 01:02:35 +0200

    start

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/thorstenkloehn/HeadlessCmst
-
-go 1.22.3
--- main.go
+++ main.go
@@ -1,25 +1,7 @@
 package main
 
-import (
-    "encoding/json"
-    "fmt"
-    "log"
-
-    "github.com/thorstenkloehn/HeadlessCmst/model"
-)
+import "fmt"
 
 func main() {
-    restaurant := model.Restaurant{
-        ID:        1,
-        Name:      "Restaurant Name",
-        Kategorie: []string{"Italian", "Pizza"},
-        Location:  "POINT(12.34 56.78)",
-    }
-
-    jsonData, err := json.Marshal(restaurant)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    fmt.Println(string(jsonData))
+ fmt.Println("Hello, World!")
 }
